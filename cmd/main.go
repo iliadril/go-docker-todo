284d183b673eba86536b30ca077dc79becf8e65f
iliadril <xiliadril@gmail.com>
2022-05-22 13:37:35 +0000

    fixed Handlers

--- cmd/main.go
+++ cmd/main.go
@@ -16,10 +16,10 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/todos", h.GetAllTodos).Methods(http.MethodGet)
-	r.HandleFunc("/books/{id}", h.GetTodo).Methods(http.MethodGet)
-	r.HandleFunc("/books", h.AddTodo).Methods(http.MethodPost)
-	r.HandleFunc("/books/{id}", h.UpdateTodo).Methods(http.MethodPut)
-	r.HandleFunc("/books/{id}", h.DeleteTodo).Methods(http.MethodDelete)
+	r.HandleFunc("/todos/{id}", h.GetTodo).Methods(http.MethodGet)
+	r.HandleFunc("/todos", h.AddTodo).Methods(http.MethodPost)
+	r.HandleFunc("/todos/{id}", h.UpdateTodo).Methods(http.MethodPut)
+	r.HandleFunc("/todos/{id}", h.DeleteTodo).Methods(http.MethodDelete)
 
 	log.Println("Starting server :8080")
 	srv := &http.Server{
